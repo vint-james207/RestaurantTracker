f61fef6dbe48a7caf6c2d81d7a8c12b5559069ab
jamesyburr <vint.james@gmail.com>
2016-06-07 11:16:58 -0400

    initial import

---
+++ src/com/james/Restaurant.java
@@ -0,0 +1,18 @@
+package com.james;
+
+/**
+ * Created by jamesyburr on 6/7/16.
+ */
+public class Restaurant {
+    String name;
+    String location;
+    int rating;
+    String comment;
+
+    public Restaurant(String name, String location, int rating, String comment) {
+        this.name = name;
+        this.location = location;
+        this.rating = rating;
+        this.comment = comment;
+    }
+}
